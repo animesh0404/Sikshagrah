43da4296bada40c6ad08d751ccbfee043cf0bc4e
Animesh Kahara <animeshcyberphysco@gmail.com>
2024-03-21 20:45:20 +0530

    Project Skeleton Created.

---
+++ src/test/java/com/kahara/sikshagrah/SikshagrahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kahara.sikshagrah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SikshagrahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
