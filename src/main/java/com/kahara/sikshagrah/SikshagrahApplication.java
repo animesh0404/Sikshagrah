ccc69f64940ed1b89e9ac02996735995bd464b76
Animesh Kahara <animeshcyberphysco@gmail.com>
2024-03-24 15:06:37 +0530

    Refactored Student Controller.
    - Refactored StudentController class for integration of dto's.
    - Formatted SikshagrahApplicatoin class.

--- src/main/java/com/kahara/sikshagrah/SikshagrahApplication.java
+++ src/main/java/com/kahara/sikshagrah/SikshagrahApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SikshagrahApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SikshagrahApplication.class, args);
 	}
-
 }
--- src/main/java/com/kahara/sikshagrah/controller/StudentController.java
+++ src/main/java/com/kahara/sikshagrah/controller/StudentController.java
@@ -1,10 +1,7 @@
 package com.kahara.sikshagrah.controller;
 
 import java.util.List;
-import java.util.Optional;
 
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,47 +11,42 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.kahara.sikshagrah.model.Student;
-import com.kahara.sikshagrah.repo.StudentRepo;
+import com.kahara.sikshagrah.dto.StudentDto;
+import com.kahara.sikshagrah.service.StudentService;
 
 @RestController
 @CrossOrigin(origins = "*")
 public class StudentController {
 
-	@Autowired
-	private StudentRepo studentRepo;
+	private final StudentService studentService;
+
+	public StudentController(StudentService studentService) {
+		this.studentService = studentService;
+	}
 
 	@GetMapping("/students")
-	List<Student> getAllStudent() {
-		return studentRepo.findAll();
+	List<StudentDto> getAllStudent() {
+		return studentService.getAllStudent();
 	}
 
 	@GetMapping("/student/{roll}")
-	Student getStudentById(@PathVariable("roll") Long roll) {
-		return studentRepo.findById(roll).get();
+	StudentDto getStudentById(@PathVariable("roll") Long roll) {
+		return studentService.getStudendByRoll(roll);
 	}
 
 	@PostMapping("/student")
-	Student addStudent(@RequestBody Student newStudent) {
-		return studentRepo.save(newStudent);
+	StudentDto addStudent(@RequestBody StudentDto newStudent) {
+		return studentService.addStudent(newStudent);
 	}
 
 	@PutMapping("/student")
-	Student updateStudent(@RequestBody Student updatedStudent) {
-		Long roll = updatedStudent.getRoll();
-		Optional<Student> existingStudent = studentRepo.findById(roll);
-		Student temp = null;
-		if (existingStudent.isPresent()) {
-			temp = existingStudent.get();
-			BeanUtils.copyProperties(updatedStudent, temp);
-			temp = studentRepo.save(temp);
-		}
-		return temp;
+	StudentDto updateStudent(@RequestBody StudentDto updatedStudent) {
+		return studentService.updateStudent(updatedStudent);
 	}
 
 	@DeleteMapping("/student/{roll}")
 	String deleteStudent(@PathVariable("roll") Long roll) {
-		studentRepo.deleteById(roll);
+		studentService.deleteStudent(roll);
 		return "Student with roll no " + roll + " deleted.";
 	}
 }
